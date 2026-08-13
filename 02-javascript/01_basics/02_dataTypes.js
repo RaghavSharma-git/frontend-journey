55e1da85d007f63d465b3d61b9298dcaef959193
RAGHAV SHARMA <gajaniradhasharma@gmail.com>
2026-08-13 22:00:16 +0530

    Add files via upload

---
+++ 02-javascript/01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello world"); we are using nodejs so alert will not work here
+
+console.log(3 + 3);
+
+console.log("raghav");
+
+let name = "raghav";
+let age = 21;
+let isLoggedIn = false;
+let state = null;
+
+//datatypes in js
+// 1. primitive data types- number, string, boolean, null, undefined, symbol
+// 2. reference data types- objects, arrays, functions
+
+console.log(typeof "raghav"); //string
+
+console.log(typeof age);//number
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object (this is a bug in js, null is not an object)
