78f77f2c7a79c9562e79c2aa68df46afb01c35a2
RAGHAV SHARMA <gajaniradhasharma@gmail.com>
2026-09-04 01:21:56 +0530

    Add files via upload

---
+++ 02-javascript/04_control_flow/15_switch.js
@@ -0,0 +1,112 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3 
+
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+        
+    case 1:
+        console.log("January");
+        break;
+            
+        
+    case 2:
+        console.log("february");
+        break;
+            break;
+    case 3:
+        console.log("march");
+        break;
+        
+    case 4:
+        console.log("april");
+        break;
+            
+        
+    case 5:
+        console.log("may");
+        break;
+        
+    case 6:
+        console.log("june");
+        break;
+        
+    case 7:
+        console.log("july");
+        break;
+
+    case 8:
+        console.log("august");
+        break;
+
+    case 9:
+        console.log("september");
+        break;
+
+    case 10:
+        console.log("october");
+        break;
+
+    case 11:
+        console.log("november");
+        break;
+
+    case 12:
+        console.log("december");
+        break;
+
+    default:
+        console.log("default case match");
+        
+    break;
+    }
+
+    //OR 
+
+    const onth = "mar"
+
+    switch (onth) {
+
+        case "jan":
+            console.log("january");
+            break;
+        case "feb":
+            console.log("february");
+            break;
+        case "mar":
+            console.log("march");
+            break;
+        case "apr":
+            console.log("april");
+            break;
+        case "may":
+            console.log("may");
+            break;
+        case "jun":
+            console.log("june");
+            break;
+        case "jul":
+            console.log("july");
+            break;
+        case "aug":
+            console.log("august");
+            break;
+        case "sep":
+            console.log("september");
+            break;
+    
+        default:
+            console.log("try again!!");
+            
+            break;
+    }
